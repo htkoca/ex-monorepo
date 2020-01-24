b48159a1221415efaf5b2e1e7140c3f4f1e901f2
Apollo Tang <tang.apollo@gmail.com>
2020-01-24 16:13:50 -0500

    [fix bug] module also need to transpile even if it does not has jsx

--- apps/nextjs-app/next.config.js
+++ apps/nextjs-app/next.config.js
@@ -2,6 +2,7 @@
 const withTM = require('next-transpile-modules')([
   // pass the modules you would like to see transpiled
   'ui--my-button',
+  'util--get-message'
 ]);
 
 module.exports = withTM();
